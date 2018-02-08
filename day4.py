bd5af8773a087839d881abffb1ac9b014eb414e7
polon59 <przemyslaw.polczak@gmail.com>
2018-02-08 23:51:18 +0100

    complete second part of 'day4' task

--- day4.py
+++ day4.py
@@ -3,30 +3,45 @@ def get_table_from_file(file_name = "day4text.txt"):
         lines = file.readlines()
     table = [element.replace("\n", "").split(" ") for element in lines]
     return table
+ 
+ 
+def sort_table(table):
+    for passpharse in table:
+        i = 0
+
+        while i < len(passpharse):
+            passpharse[i] = ''.join(sorted(passpharse[i]))
+            i+=1
+
+    return table
 
 
 def count_elements_with_duplicates(table):
     invalid = 0
     valid = 0
+
     for passphrase in table:
         unique_passpharse = list(set(passphrase))
         unique_passpharse = sorted(unique_passpharse)
         passphrase = sorted(passphrase)
-
+ 
         if unique_passpharse != passphrase:
             invalid += 1
         else:
             valid += 1
-
+ 
     print("valid passpharses: " + str(valid))
     print("invalid passpharses: " + str(invalid))
-
+ 
 def main():
     table = get_table_from_file(file_name = "day4text.txt")
-    invalid = count_elements_with_duplicates(table)
-
-
+    table = sort_table(table) #comment this line to do not get anagrams into account
+    count_elements_with_duplicates(table)
+ 
+ 
 main()
+ 
+ 
 
 
 
